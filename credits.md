e19d5dda95721df92b5373c74c42907ce8e4a077
Stanislas  Dolcini <stanislas.dolcini@avanade.com>
2019-12-06 08:20:19 +0100

    Base interface + music and credits

---
+++ credits.md
@@ -0,0 +1,6 @@
+# Credits
+
+## Music
+
+Menu theme, battle theme:
+- Alexandr Zhelanov https://soundcloud.com/alexandr-zhelanov
---
+++ gui/pregame/backgrounds/stella_artis_01.js
@@ -0,0 +1,8 @@
+g_BackgroundLayerData.push(
+    [
+        {
+            "offset": (time, width) => 0,
+            "sprite": "stella_artis_01",
+            "tiling": true,
+        },
+    ]);
